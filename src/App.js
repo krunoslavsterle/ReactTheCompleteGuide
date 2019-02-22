4044db91bf41eb8e75a9beddae034fc3182678ce
ksterle <krunoslavsterle@gmail.com>
2019-02-22 19:05:00 +0100

    - media query

--- src/App.js
+++ src/App.js
@@ -59,16 +59,10 @@ class App extends Component {
     }
 
     render() {
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        };
-
         let persons = null;
+        let btnClass = '';
+
+
         if (this.state.showPersons) {
             persons = (
                 <div> 
@@ -83,7 +77,7 @@ class App extends Component {
                 </div>
             );
 
-            style.backgroundColor = 'red';          
+            btnClass = classes.Red;
         }
 
         const assignedClasses = [];
@@ -100,8 +94,11 @@ class App extends Component {
                 <h1>Hi, I'm a React App</h1>   
                 <p className={assignedClasses.join(' ')}>This is really working</p>
                     <button 
-                        style={style}
-                        onClick={() => this.togglePersonsHandler()}>Switch Name</button>
+                        className={btnClass}
+                        onClick={() => this.togglePersonsHandler()}
+                    >
+                        Switch Name
+                    </button>
                     
                     {/* { this.state.showPersons ? // This is one way of handling conditions, not the best way
                         <div>
