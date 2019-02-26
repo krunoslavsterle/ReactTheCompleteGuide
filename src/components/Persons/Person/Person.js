04cc08ed16ce7fbe8d6dae2a12b3f5a8d9e11752
ksterle <krunoslavsterle@gmail.com>
2019-02-26 20:03:21 +0100

    - implementing React.createRef();

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,6 +3,17 @@ import classes from "./Person.css";
 import withClass from "../../../hoc/withClass";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    //this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log("[Person.js] rendering....");
     return (
@@ -12,6 +23,10 @@ class Person extends Component {
         </p>
         <p>{this.props.children}</p>
         <input
+          // ref={inputEl => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
