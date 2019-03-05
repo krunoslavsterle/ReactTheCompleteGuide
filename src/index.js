de5b9b81f155f032c6b40b96d48c120b8b53b066
ksterle <krunoslavsterle@gmail.com>
2019-03-05 20:26:30 +0100

    - react lazy; suspense

--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+import './index.css';
+import App from './App';
 
-axios.interceptors.request.use(
-  request => {
-    console.log(request);
-    return request;
-  },
-  error => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-axios.interceptors.response.use(
-  response => {
-    console.log(response);
-    return response;
-  },
-  error => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
